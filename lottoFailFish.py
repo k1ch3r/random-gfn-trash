ba1f9a6cd430b0538577715e2bb33bcb0783138e
Jonas Ewert <JonasEw@gmail.com>
2023-10-12 10:50:17 +0200

    trying to document shit

--- lottoFailFish.py
+++ lottoFailFish.py
@@ -2,17 +2,22 @@
  
 import random
 
-def generateArray(rangeSize):
+def generateArray(rangeSize: int):
+    '''returns an Array with integer elements from 1 to rangeSize'''
     returnArray = []
     for count in range(rangeSize):
         returnArray.append(count + 1)
     return returnArray
 
-def drawFromArray(arrayToAppend, arrayToDrawFrom):
+def drawFromArray(arrayToAppend: str, arrayToDrawFrom: str):
+    '''removes elements from arrayToDrawFrom and appends them to arrayToAppend'''
     arrayToAppend.append(arrayToDrawFrom.pop(random.randint(0, len(arrayToDrawFrom) - 1)))
     return arrayToAppend
 
-def drawNumbers(numberCount, rangeSize):
+def drawNumbers(numberCount: int, rangeSize: int):
+    '''draws numberCount elements from a pool of numbers in a range from 1 to rangeSize without duplicates and puts them in an array.
+    
+    returns the generated array.'''
     numbersToReturn = []
     poolToDraw = generateArray(rangeSize)
     for numberDrawn in range(numberCount):
@@ -28,9 +33,12 @@ print(lotteryNumbers)
 
 import random
 
-def drawNumbers(numberCount, rangeSize):
+def drawNumbers(numberCount: int, rangeSize: int):
+    '''returns an array containing numberCount integer elements in a range from 1 to rangeSize without duplicates.'''
     poolToDraw = list(range(1, rangeSize + 1))
     return [poolToDraw.pop(random.randint(0, len(poolToDraw) - 1)) for _ in range(numberCount)]
 
 lotteryNumbers = drawNumbers(6, 49)
 print(lotteryNumbers)
+
+#usage
